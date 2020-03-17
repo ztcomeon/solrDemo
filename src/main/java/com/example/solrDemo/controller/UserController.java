ff0b8f7111756a0077bf96c7db05744e78de965b
zhangtianxin <zhangtianxin@vanda-tec.cn>
2020-03-17 16:20:24 +0800

    新增

--- src/main/java/com/example/solrDemo/controller/UserController.java
+++ src/main/java/com/example/solrDemo/controller/UserController.java
@@ -13,14 +13,17 @@ package com.example.solrDemo.controller;
 import com.example.solrDemo.controller.model.ResponseCode;
 import com.example.solrDemo.controller.model.ResponseModel;
 import com.example.solrDemo.entity.UserEntity;
+import com.example.solrDemo.service.SolrService;
 import com.example.solrDemo.service.UserService;
 import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.response.UpdateResponse;
 import org.apache.solr.common.SolrInputDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.management.Query;
 import java.util.Date;
 import java.util.List;
 
@@ -54,6 +57,7 @@ public class UserController extends BaseController {
             doc.setField("name", u.getUserName());
 
             client.add(doc);//如果配置文件中没有指定core，这个方法的第一个参数就需要指定core名称,比如client.add("xjxcc", doc);
+
             client.commit();//如果配置文件中没有指定core，这个方法的第一个参数就需要指定core名称client.commit("xjxcc");
             return new ResponseModel(new Date().getTime(), null, ResponseCode._200, "");
         } catch (Exception e) {
@@ -61,12 +65,18 @@ public class UserController extends BaseController {
         }
     }
 
+    @Autowired
+    SolrService solrService;
+
     @RequestMapping(value = "/findByName", method = {RequestMethod.GET})
     public ResponseModel findByName(String name) {
         try {
-            List<UserEntity> userList = userService.findByName(name);
+//            List<UserEntity> userList = userService.findByName(name);
+//            UpdateResponse updateResponse = client.deleteByQuery("id:"+"ed327f46-46b6-4a77-96a9-b8a14080b8a1");
+//            client.commit();
+//            System.out.println(updateResponse);
 
-            return new ResponseModel(new Date().getTime(), userList, ResponseCode._200, "");
+            return new ResponseModel(new Date().getTime(), null, ResponseCode._200, "");
         } catch (Exception e) {
             return this.buildHttpReslutForException(e);
         }
